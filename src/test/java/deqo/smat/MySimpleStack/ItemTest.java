7fd07a7ce03c9baa7e3e62d4537ad2f32bb5ca22
polybutyleneterephthalate <stefano.matthia@gmail.com>
2017-10-19 11:54:05 +0200

    JUnit test creation for MySimpleStack

---
+++ src/test/java/deqo/smat/MySimpleStack/ItemTest.java
@@ -0,0 +1,16 @@
+package deqo.smat.MySimpleStack;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ItemTest {
+    @Test
+    public void getValue() throws Exception {
+    }
+
+    @Test
+    public void setValue() throws Exception {
+    }
+
+}
---
+++ src/test/java/deqo/smat/MySimpleStack/SimpleStackImplTest.java
@@ -0,0 +1,28 @@
+package deqo.smat.MySimpleStack;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SimpleStackImplTest {
+    @Test
+    public void isEmpty() throws Exception {
+    }
+
+    @Test
+    public void getSize() throws Exception {
+    }
+
+    @Test
+    public void push() throws Exception {
+    }
+
+    @Test
+    public void peek() throws Exception {
+    }
+
+    @Test
+    public void pop() throws Exception {
+    }
+
+}
