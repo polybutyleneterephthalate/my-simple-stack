bfba80327c5d761c5667fe3e18f6573688d2b86c
polybutyleneterephthalate <stefano.matthia@gmail.com>
2017-10-27 11:34:23 +0200

    adding mockito tests for class Item (not functionnal)

--- src/test/java/deqo/smat/MySimpleStack/ItemTest.java
+++ src/test/java/deqo/smat/MySimpleStack/ItemTest.java
@@ -1,17 +1,29 @@
 package deqo.smat.MySimpleStack;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
+import static com.sun.javaws.JnlpxArgs.verify;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
 public class ItemTest {
+
+    //Item value = mock(Item.class);
+    Item value = new Item("");
+
     @Test
     public void getValue() throws Exception {
-
+        //Mockito.verify(value).setValue("test");
+        value.setValue("test");
+        assertEquals(value.getValue(), "test");
     }
 
     @Test
     public void setValue() throws Exception {
+        //Mockito.verify(value).setValue("test");
+        value.setValue("test");
+        assertEquals(value.getValue(), "test");
     }
 
 }
